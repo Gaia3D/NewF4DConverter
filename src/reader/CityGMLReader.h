4274ad6affe4ff738b0f162e77f5c56214c8d0f9
bcbae <bcbae@gaia3d.com>
2018-08-29 18:24:48 +0900

    CityGML 로더 작성

---
+++ src/reader/CityGMLReader.h
@@ -0,0 +1,25 @@
+﻿/**
+* CityGMLReader Header
+*/
+#ifndef _CITYGMLREADER_H_
+#define _CITYGMLREADER_H_
+#pragma once
+
+#include "Reader.h"
+
+class CityGMLReader : public Reader
+{
+public:
+	CityGMLReader();
+	virtual ~CityGMLReader();
+
+public:
+	virtual bool readRawDataFile(std::string& filePath);
+
+	virtual void clear();
+
+private:
+	bool bUseMaxLODOnly;
+};
+
+#endif // _CITYGMLREADER_H_
