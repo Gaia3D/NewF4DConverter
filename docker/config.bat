7daeb927eb41c779201c512682ded88c76a7319a
bcbae <bcbae@gaia3d.com>
2021-01-22 12:39:10 +0900

    docker build

--- docker/config.bat
+++ docker/config.bat
@@ -3,16 +3,18 @@
 REM Docker organization to pull the images from
 SET ORG=gaia3d
 
-REM  Name of image
-SET IMG=centos
-
-REM Docker TAG
-SET TAG=dev
-
-REM Docker Image
-SET IMAGE="%ORG%/%IMG%:%TAG%"
-
 REM Container
 SET CONTAINER=mago3d
 
-SET PORT_SSH=20022
+REM  Name of image
+SET IMG=centos
+
+REM root image version
+SET IMG_VER=centos7
+
+REM Docker TAG
+SET TAG=latest
+
+REM Docker Image
+SET IMAGE="%ORG%/%CONTAINER%-converter:%IMG%-%IMG_VER%-%TAG%"
+SET IMAGE_LATEST="%ORG%/%CONTAINER%-converter"
